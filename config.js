3c5a7a8bd958375e6c2fad7c4342646e97cac6be
yowasap <darrenrobinragoonath@gmail.com>
2020-10-14 15:06:15 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+/*
+* Theasys
+* Generated on: Wednesday 7th of October 2020 09:22:15 PM
+* Config file: You can change the path to match your server path.
+*/
+
+var vars = {
+    file : 'index.html',
+    path : 'C:\Users\Hermione\Desktop\Museum Tour First Half PAID FOR
+    theme : 'theasys',
+};
