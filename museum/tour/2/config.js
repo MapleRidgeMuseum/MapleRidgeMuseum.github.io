43d7398eafdc1033d2379e221193952717e01f0c
yowasap <darrenrobinragoonath@gmail.com>
2020-11-18 18:31:22 -0800

    added second tour and reuploaded first tour with links

---
+++ museum/tour/2/config.js
@@ -0,0 +1,11 @@
+/*
+* Theasys
+* Generated on: Wednesday 18th of November 2020 08:30:14 PM
+* Config file: You can change the path to match your server path.
+*/
+
+var vars = {
+    file: 'index.html',
+    path: 'https://yowasap.github.io/museum/tour/2',
+    theme: 'theasys',
+};
