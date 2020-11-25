bfe365d9e9d42343a611fd25df3031f570728f94
yowasap <darrenrobinragoonath@gmail.com>
2020-11-25 09:41:24 -0800

    Whack

--- museum/tour/1/config.js
+++ museum/tour/1/config.js
@@ -1,6 +1,6 @@
 /*
 * Theasys
-* Generated on: Wednesday 25th of November 2020 05:12:19 PM
+* Generated on: Wednesday 25th of November 2020 05:34:06 PM
 * Config file: You can change the path to match your server path.
 */
 
--- museum/tour/2/config.js
+++ museum/tour/2/config.js
@@ -1,6 +1,6 @@
 /*
 * Theasys
-* Generated on: Wednesday 25th of November 2020 05:20:12 PM
+* Generated on: Wednesday 25th of November 2020 05:36:10 PM
 * Config file: You can change the path to match your server path.
 */
 
